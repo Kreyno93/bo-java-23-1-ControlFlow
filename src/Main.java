ec2d6d107925271ae4ebf5cf7b1a4f4d69a1860a
Michael <michaelw.apple@icloud.com>
2023-04-18 17:04:07 +0200

    adding for loop example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("You finde the different variations on the Branches!");
+        forLoopExample();
 
+        forLoopExampleWithArray();
+
+    }
+
+    public static void forLoopExample() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static void forLoopExampleWithArray() {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("numbers[" + i + "] = " + numbers[i]);
+        }
     }
 }
