719f448da99871e37c8c7a539ca34955c7f09175
Michael <michaelw.apple@icloud.com>
2023-04-18 17:09:59 +0200

    adding while example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("You finde the different variations on the Branches!");
+        whileExample();
 
+        whileWithArray();
+    }
+
+    public static void whileExample() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void whileWithArray() {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int i = 0;
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            i++;
+        }
     }
 }
