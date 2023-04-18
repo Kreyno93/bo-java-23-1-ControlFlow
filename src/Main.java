a62d11a23f49c3b5de895c72592a3412e3af2bf2
Michael <michaelw.apple@icloud.com>
2023-04-18 17:08:14 +0200

    adding switchCase Example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,56 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("You finde the different variations on the Branches!");
 
+        switchCaseExample();
+
+        switchCaseWithLambda();
+
+    }
+
+    public static void switchCaseExample() {
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+
+
+    // FANCY SWITCH CASE VARIATION
+    public static void switchCaseWithLambda() {
+        int day = 4;
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid day");
+        }
     }
 }
